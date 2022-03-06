9fda637e43fcd1b2bdadc9eeb4d430b9de8934b2
qijizh <qijizh1999@163.com>
2022-03-06 14:40:34 +0800

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+// export_function("listJobLog", "/monitor/jobLog/list", "get", query);
+// export_function("delJobLog", "/monitor/jobLog/jobLog", "delete");
+// export_function("cleanJobLog", "/monitor/jobLog/clean", "delete");
+
+let data = [
+  {
+    name: "listJobLog",
+    arg: "query",
+    url: "'/monitor/jobLog/list'",
+    method: "get",
+    param: "query",
+    comment: "查询调度日志列表",
+  },
+  {
+    name: "delJobLog",
+    arg: "jobLogId",
+    url: "'/monitor/jobLog/'+jobLog",
+    method: "delete",
+    comment: "删除调度日志",
+  },
+  {
+    name: "cleanJobLog",
+    url: "'/monitor/jobLog/clean'",
+    method: "delete",
+    comment: "删除调度日志",
+  },
+];
+
+const ejs = require("ejs");
+const fs = require("fs");
+const path = require("path");
+ejs.renderFile("./temp.ejs", { data }, (err, str) => {
+  fs.writeFile(path.join(__dirname, "funs.js"), str, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("success");
+    }
+  });
+});
